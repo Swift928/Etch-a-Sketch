04c549a10d2f1a7d79db7a28e8964cf1b7bb6a59
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2023-08-22 03:35:38 -0500

    added reset button, styling, tidy code

--- script.js
+++ script.js
@@ -1,40 +1,73 @@
 
-let divContainer = document.createElement('div');
-divContainer.setAttribute('id', 'etchContainer')
-
-let promptButton = document.querySelector('#user-input');
-
+let gridContainer = document.createElement('div');
+gridContainer.setAttribute('id', 'etchContainer')
+let dimensionButton = document.querySelector('#user-input');
 let dimensionSize;
-
-document.body.append(divContainer);
+let resetButton = document.querySelector('#reset')
+document.body.append(gridContainer);
 let originalColor = getComputedStyle(document.documentElement).getPropertyValue('--original-color')
+
+// Create initial grid
+function createGrid() {
+    for (i=0; i < 16; i++) {
+        for (j=0; j < 16; j++) {
+            var divColumns = document.createElement('div')
+            divColumns.classList.add('etchElement');
+            gridContainer.appendChild(divColumns)
+
+            divColumns.addEventListener('mouseover', (event) => {
+                changeBackgroundColor(event.target);
+                
+            });
+        }}}
+createGrid()
+
 
 function randomRGB() {
     let r = Math.floor(Math.random() * 256)
     let g = Math.floor(Math.random() * 256)
     let b = Math.floor(Math.random() * 256)
-    return `rgb(${r}, ${g}, ${b})`
-}
+    return `rgb(${r}, ${g}, ${b})`}
+
 
 function changeBackgroundColor(element) {
     element.style.backgroundColor = randomRGB()};
 
-  promptButton.addEventListener('click', () => {
+
+// Clear the grid colors
+resetButton.addEventListener('click', () => {
+    let children = gridContainer.querySelectorAll('.etchElement')
+    children.forEach(child => {
+        child.style.backgroundColor = originalColor
+    })
+})
+
+  dimensionButton.addEventListener('click', () => {
         dimensionSize = prompt(`Please enter a dimension you would like to see: `)
-        divContainer.style.setProperty('--grid-numb', dimensionSize)
-        divContainer.innerHTML = ''
-        
+
+        while (dimensionSize !== null) {
+            if (dimensionSize >= 2 && dimensionSize <= 100) {
+                gridContainer.innerHTML = ''
+                gridContainer.style.gridTemplateColumns = `repeat(${dimensionSize}, 2fr)`
+                gridContainer.style.gridTemplateRows = `repeat(${dimensionSize}, 2fr)`                               
     
-        for (i=0; i<dimensionSize; i++) {
-            for (j=0; j<dimensionSize; j++) {
-                var divColumns = document.createElement('div')
-                divColumns.classList.add('etchElement');
-                divContainer.appendChild(divColumns)
+                for (i=0; i<dimensionSize; i++) {
+                    for (j=0; j<dimensionSize; j++) {
+                        var divColumns = document.createElement('div')
+                        divColumns.classList.add('etchElement');
+                        gridContainer.appendChild(divColumns)
 
-                divColumns.addEventListener('mouseover', (event) => {
-                    changeBackgroundColor(event.target);
-                    
-                });
-            }}
-            });
-
+                        divColumns.addEventListener('mouseover', (event) => {
+                            changeBackgroundColor(event.target); 
+                        });
+                    }} break;
+            } else if (dimensionSize > 100) {
+                alert('Please enter a number less than 101!')
+                dimensionSize = prompt(`Please enter a dimension you would like to see: `)
+            } else if (dimensionSize < 2)  {
+                alert('Please enter a number greater than 1!')
+                dimensionSize = prompt(`Please enter a dimension you would like to see: `)
+            } else { dimensionSize = prompt(`Please enter a dimension you would like to see: `)}
+        }
+        });
+        
