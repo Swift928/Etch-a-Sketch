01c4cf53d1e8e6b7803e4b89061e1062f5d39253
Keith Taylor <68087065+Swift928@users.noreply.github.com>
2023-08-21 21:30:18 -0500

    added dynamic grid, now using grid not flexbox

--- script.js
+++ script.js
@@ -4,23 +4,13 @@ contentContainer.setAttribute('id', 'container')
 let divContainer = document.createElement('div');
 divContainer.setAttribute('id', 'etchContainer')
 
-for (i=0; i<16; i++) {
-    let divRows = document.createElement('div');
-    divRows.classList.add('divRows');
-    for (j=0; j<16; j++) {
-        let divColumns = document.createElement('div')
-        divColumns.classList.add('etchElement');
-        divRows.appendChild(divColumns);
-    }
-    divContainer.appendChild(divRows)
-}
+let promptButton = document.querySelector('#user-input');
+
+let dimensionSize;
 
 contentContainer.appendChild(divContainer)
-
-document.body.appendChild(contentContainer);
-
-let divRows = document.querySelectorAll('.etchElement')
-
+document.body.append(contentContainer);
+let originalColor = getComputedStyle(document.documentElement).getPropertyValue('--original-color')
 
 function randomRGB() {
     let r = Math.floor(Math.random() * 256)
@@ -29,15 +19,31 @@ function randomRGB() {
     return `rgb(${r}, ${g}, ${b})`
 }
 
-divRows.forEach(item => {
+  promptButton.addEventListener('click', () => {
+        dimensionSize = prompt(`Please enter a dimension you would like to see: `)
+        divContainer.style.setProperty('--grid-numb', dimensionSize)
+        divContainer.innerHTML = ''
     
-    const originalColor = getComputedStyle(document.documentElement).getPropertyValue('--original-color')
-    
-        item.addEventListener('mouseenter', () => {
-            item.style.backgroundColor = randomRGB()
-        });
+        for (i=0; i<dimensionSize; i++) {
+            for (j=0; j<dimensionSize; j++) {
+                var divColumns = document.createElement('div')
+                divColumns.classList.add('etchElement');
+                divContainer.appendChild(divColumns)
 
-        item.addEventListener('mouseleave', () => {
-            item.style.backgroundColor = originalColor /* Using the stored 'var' variable to revert back to original color */
-        });
-    });
+                divColumns.addEventListener('mouseenter', (event) => {
+                    changeBackgroundColor(event.target);
+                    
+                });
+                divColumns.addEventListener('mouseleave', (event) => {
+                    revertBackgroundColor(event.target);
+                    console.log(event.target)
+                });
+            }}
+
+            function changeBackgroundColor(element) {
+                element.style.backgroundColor = randomRGB()};
+
+            function revertBackgroundColor(element) {
+                element.style.backgroundColor = originalColor};
+            });
+
